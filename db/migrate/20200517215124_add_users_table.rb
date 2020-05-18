860866cc261c42afb039328d25d4553ffce8a7e2
arturo <marksandobal@gmail.com>
2020-05-17 18:35:52 -0700

    Add user model

---
+++ db/migrate/20200517215124_add_users_table.rb
@@ -0,0 +1,12 @@
+class AddUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
