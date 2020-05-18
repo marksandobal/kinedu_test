6312cb4fc1148ad541ffc1f706bb80fd8b583e3f
arturo <marksandobal@gmail.com>
2020-05-18 14:14:04 -0700

    update json web token class

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_215124) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
